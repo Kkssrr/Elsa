72f3e079f16ff8c2f1b61ffb5e9f8eba45554e22
Kkssrr <109816139+Kkssrr@users.noreply.github.com>
2023-05-22 18:13:50 +0530

    Update info.py

--- info.py
+++ info.py
@@ -75,10 +75,10 @@ TMP_DOWNLOAD_DIRECTORY = environ.get("TMP_DOWNLOAD_DIRECTORY", "./DOWNLOADS/")
 COMMAND_HAND_LER = environ.get("COMMAND_HAND_LER", "/")
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI',"mongodb+srv://pritamgang4:WE80Y5BTAеpcSmWP@cluster0.zjiqwba.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URI = environ.get('DATABASE_URI',"mongodb+srv://pritamgang42:oJVkz1lgCl4mXiVu@cluster0.dn1xzvu.mongodb.net/?retryWrites=true&w=majority")
 
 
-DATABASE_NAME = environ.get('DATABASE_NAME', "pritamgang4")
+DATABASE_NAME = environ.get('DATABASE_NAME', "smart")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 MONGO_URL = os.environ.get('MONGO_URL',)
 
