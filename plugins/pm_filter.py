62114af8d901cf85b48bddc01514b7ff2c628784
Kkssrr <109816139+Kkssrr@users.noreply.github.com>
2023-07-24 07:19:44 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1227,21 +1227,21 @@ async def auto_filter(client, msg, spoll=False):
                 ]
                 for file in files
             ]
-
-         btn.insert(0, 
-        [
-            InlineKeyboardButton(f' ♀️ {search} ♀️ ', 'qinfo')
-        ]
-              )
+      btn.insert(0, 
+          [
+             InlineKeyboardButton(f' ♀️ {search} ♀️ ', 'qinfo')
+          ]
                  
+      )            
       btn.insert(1, 
-         [
+          [
              InlineKeyboardButton(f'ɪɴꜰᴏ', 'reqinfo'),
              InlineKeyboardButton(f'ᴍᴏᴠɪᴇ', 'minfo'),
              InlineKeyboardButton(f'sᴇʀɪᴇs', 'sinfo'),
              InlineKeyboardButton(f'ᴛɪᴘs', 'tinfo')
-         ]
-              )
+          ]
+                
+     )          
 
       #if ENABLE_TUTORIAL == True:
         #btn.insert(0, [
